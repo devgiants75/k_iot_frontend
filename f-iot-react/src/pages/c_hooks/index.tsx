ac0b74ffd318a874dfd14561e33773b2b90454de
devgiants75 <devgiants75@naver.com>
2025-11-04 10:35:17 +0900

    20251104 React Hooks useRef

--- f-iot-react/src/pages/c_hooks/index.tsx
+++ f-iot-react/src/pages/c_hooks/index.tsx
@@ -8,6 +8,8 @@ import State06 from './a_useState/State06'
 import State06_Child from './a_useState/State06_Child'
 import Ref01 from './b_useRef/Ref01'
 import Ref02 from './b_useRef/Ref02'
+import Practice01 from './b_useRef/Practice01'
+import Practice02 from './b_useRef/Practice02'
 
 const h2Style = {
   backgroundColor: 'black',
@@ -23,6 +25,8 @@ function Index() {
       <h2 style={h2Style}>2. 리액트 Hooks - useRef</h2>
       <Ref01 /> <hr />
       <Ref02 /> <hr />
+      <Practice01 /> <hr />
+      <Practice02 /> <hr />
 
       <h2 style={h2Style}>1. 리액트 Hooks - useState</h2>
       <State01 /> <hr />
