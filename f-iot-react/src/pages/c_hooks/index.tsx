39ce2afd52e624e129668a9e97b6cf0ec0153f7c
devgiants75 <devgiants75@naver.com>
2025-11-04 12:48:36 +0900

    20251104 React Hooks useEffect

--- f-iot-react/src/pages/c_hooks/index.tsx
+++ f-iot-react/src/pages/c_hooks/index.tsx
@@ -5,13 +5,13 @@ import State03 from './a_useState/State03'
 import State04 from './a_useState/State04'
 import State05 from './a_useState/State05'
 import State06 from './a_useState/State06'
-import State06_Child from './a_useState/State06_Child'
 import Ref01 from './b_useRef/Ref01'
 import Ref02 from './b_useRef/Ref02'
-import Practice01 from './b_useRef/Practice01'
-import Practice02 from './b_useRef/Practice02'
+import Ref_Practice01 from './b_useRef/Practice01'
+import Ref_Practice02 from './b_useRef/Practice02'
 import Effect01 from './c_useEffect/Effect01'
 import Effect02 from './c_useEffect/Effect02'
+import Effect_Practice01 from './c_useEffect/Practice01'
 
 const h2Style = {
   backgroundColor: 'black',
@@ -27,12 +27,13 @@ function Index() {
       <h2 style={h2Style}>3. 리액트 Hooks - useEffect</h2>
       <Effect01 /> <hr />
       <Effect02 /> <hr />
+      <Effect_Practice01 /> 
 
       <h2 style={h2Style}>2. 리액트 Hooks - useRef</h2>
       <Ref01 /> <hr />
       <Ref02 /> <hr />
-      <Practice01 /> <hr />
-      <Practice02 /> <hr />
+      <Ref_Practice01 /> <hr />
+      <Ref_Practice02 /> <hr />
 
       <h2 style={h2Style}>1. 리액트 Hooks - useState</h2>
       <State01 /> <hr />
