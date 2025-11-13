886c75caa494ff6ef45a1176093a0e7fb53c2041
devgiants75 <devgiants75@naver.com>
2025-11-13 10:49:41 +0900

    20251113 react style

---
+++ f-iot-react/src/pages/f_style/index.tsx
@@ -0,0 +1,24 @@
+import React from 'react'
+import ToggleSection from '@/components/ToggleSection'
+import A_CSS from './A_CSS'
+import B_Module from './B_Module'
+
+function Index() {
+  return (
+    <div>
+      <h1 style={{ backgroundColor: "black", color: "white" }}>
+        === 리액트 스타일(Style) ===
+      </h1>
+
+      <ToggleSection title="1. 일반 CSS">
+        <A_CSS />
+      </ToggleSection>
+      
+      <ToggleSection title="2. Module CSS">
+        <B_Module />
+      </ToggleSection>
+    </div>
+  )
+}
+
+export default Index
