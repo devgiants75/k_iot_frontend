ba568fb6002b8362822945cf350ce57f0d17ca01
devgiants75 <devgiants75@naver.com>
2025-11-13 14:51:49 +0900

    20251113 react style Login

--- f-iot-react/src/pages/f_style/index.tsx
+++ f-iot-react/src/pages/f_style/index.tsx
@@ -3,6 +3,8 @@ import ToggleSection from '@/components/ToggleSection'
 import A_CSS from './A_CSS'
 import B_Module from './B_Module'
 import C_StyleComponents from './C_StyleComponents'
+import D_Emotion from './D_Emotion'
+import D_EmotionPractice from './emotion-practice/EmotionPractice'
 
 function Index() {
   return (
@@ -21,6 +23,14 @@ function Index() {
       
       <ToggleSection title="3. Styled Components">
         <C_StyleComponents />
+      </ToggleSection>
+
+      <ToggleSection title="4. Emotion">
+        <D_Emotion />
+      </ToggleSection>
+
+      <ToggleSection title="5. Emotion + 반응형">
+        <D_EmotionPractice />
       </ToggleSection>
     </div>
   )
