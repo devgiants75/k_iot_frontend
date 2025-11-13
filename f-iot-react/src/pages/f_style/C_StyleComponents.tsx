f93444a86a906be7ba519e6b7b67fc9234244cb8
devgiants75 <devgiants75@naver.com>
2025-11-13 12:46:25 +0900

    20251113 react style Login

--- f-iot-react/src/pages/f_style/C_StyleComponents.tsx
+++ f-iot-react/src/pages/f_style/C_StyleComponents.tsx
@@ -53,11 +53,111 @@ export const theme = {
 // ctrl + shift + p
 // typescript: restart ts server
 
+const LoginContainer = styled.div`
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  height: 100vh;
+  background: linear-gradient(160deg, #f6d365 0%, #fda085 100%);
+  /* 
+    @ clamp: CSS 함수 
+    : 값을 최소값, 최대값 사이로 제한(clamp)
+    - 가운데 인자인 권장값을 계산하여
+      , 그 값이 최소보다 작으면 최소값을 최대보다 크면 최대값을
+      , 그렇지 않으면 그 권장값을 사용
+
+      ^ vw 단위: viewport width의 약자 (viewport 너비 대비 퍼센트 수치)
+  */
+  padding: clamp(1rem, 5vw, 3rem);
+`;
+
+const LoginCard = styled.div`
+  display: grid;
+  gap: 1.5rem;
+  background-color: #fff;
+  padding: clamp(2rem, 5vw, 4rem);
+  width: min-content(100%, 420px);
+  border: 1px solid #ddd;
+  border-radius: var(--radius-md);
+  /* align-items + justify-items */
+  place-items: center;
+`;
+
+const Title = styled.h1`
+  font-size: clamp(1.5rem, 3vw, 2rem);
+  font-weight: 700;
+  color: #333;
+`;
+
+const Subtitle = styled.p`
+  font-size: clamp(0.9rem, 2vw, 1rem);
+  color: #666;
+  text-align: center;
+`;
+
+const Form = styled.form`
+  display: grid;
+  gap: 1rem;
+  width: 100%;
+`;
+
+const Input = styled.input`
+  padding: 0.9rem 1rem;
+  border: 1px solid #ddd;
+  border-radius: 0.8rem;
+  font-size: 16px;
+  width: 100%;
+  transition: border 0.2s;
+
+  &:focus {
+    border-color: #fda085;
+    outline: none;
+  }
+`;
+const LoginButton = styled.button`
+  padding: 0.9rem;
+  background-color: #fda085;
+  color: white;
+  border: none;
+  border-radius: 0.85rem;
+  transition: background 0.2s;
+  
+  &:hover {
+    background: #f6d365;
+  }
+`;
+const FooterText = styled.p`
+  font-size: 0.9rem;
+  color: #888;
+  text-align: center;
+`;
+
+export function LoginPage() {
+  return (
+    <LoginContainer>
+      <LoginCard>
+        <Title>로그인</Title>
+        <Subtitle>로그인하여 웹 페이지를 활용해보세요.</Subtitle>
+        <Form>
+          <Input type='email' placeholder='이메일' />
+          <Input type='password' placeholder='비밀번호' />
+          <LoginButton>로그인</LoginButton>
+        </Form>
+        <FooterText>계정이 없으신가요? 회원가입</FooterText>
+      </LoginCard>
+    </LoginContainer>
+  );
+}
+
 function C_StyleComponents() {
   return (
-    <Container>
-      <Button>스타일드 컴포넌트를 사용한 버튼</Button>
-    </Container>
+    <>
+      <Container>
+        <Button>스타일드 컴포넌트를 사용한 버튼</Button>
+      </Container>
+      <hr />
+      <LoginPage />
+    </>
   )
 }
 
